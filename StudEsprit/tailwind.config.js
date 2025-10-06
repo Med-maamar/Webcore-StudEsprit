993c4ad5eba30bf32ef2fd9740f9b5eae129357c
MD2108 <80361001+MD2108@users.noreply.github.com>
2025-10-06 15:32:33 +0100

    frontend update

--- StudEsprit/tailwind.config.js
+++ StudEsprit/tailwind.config.js
@@ -9,18 +9,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: {
-          DEFAULT: '#10b981',
-          50: '#ecfdf5',
-          100: '#d1fae5',
-          200: '#a7f3d0',
-          300: '#6ee7b7',
-          400: '#34d399',
-          500: '#10b981',
-          600: '#059669',
-          700: '#047857',
-          800: '#065f46',
-          900: '#064e3b',
+        esprit: {
+          red: '#c8102e',
+          dark: '#121217',
+          light: '#f8f9fb',
+          gray: '#4b5563',
         },
       },
       borderRadius: {
@@ -30,4 +23,3 @@ module.exports = {
   },
   plugins: [require('flowbite/plugin')],
 }
-
